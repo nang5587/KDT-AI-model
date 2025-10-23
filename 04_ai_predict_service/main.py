7728d8a6dcf4b9d77e2a5d0d144547bb324cc228
Hong jimin <hsk3232@pusan.ac.kr>
2025-10-23 23:47:55 +0900

    10/23

--- 04_ai_predict_service/main.py
+++ 04_ai_predict_service/main.py
@@ -1,13 +1,14 @@
 # 04_ai_predict_service/main.py
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from .artifacts import ARTS
 from .settings import settings
 from .router_anomaly import router as anomaly_router
 
-app = FastAPI(title="AI Anomaly Service", version=settings.version)
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     ARTS.load()
+    yield
 
+app = FastAPI(title="AI Anomaly Service", version=settings.version, lifespan=lifespan)
 app.include_router(anomaly_router)
